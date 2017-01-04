bc9e9459046630e5f6e66844609e0ee827a08ed2
Rafał Dziuryk <rafal.dziuryk@siroccomobile.com>
2017-01-04 23:56:59 +0100

    realm implementation

---
+++ chopperrealm/src/main/java/com/levelapp/chopper/RealmChopperable.java
@@ -0,0 +1,18 @@
+package com.levelapp.chopper;
+
+import com.levelapp.annotation.Chopperable;
+import io.realm.Realm;
+
+/**
+ * Created by rafaldziuryk on 03.01.17.
+ */
+
+public class RealmChopperable implements Chopperable<Realm> {
+
+  @Override
+  public void chopp(Realm chopp) {
+    if (chopp != null){
+      chopp.close();
+    }
+  }
+}
