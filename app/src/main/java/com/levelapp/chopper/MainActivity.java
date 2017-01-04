2acf17c3e1027380da02004c850b6ff8b5c1ee2f
Rafał Dziuryk <rafal.dziuryk@siroccomobile.com>
2017-01-04 16:35:03 +0100

    refactor + add realm

--- app/src/main/java/com/levelapp/chopper/MainActivity.java
+++ app/src/main/java/com/levelapp/chopper/MainActivity.java
@@ -3,37 +3,83 @@ package com.levelapp.chopper;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import com.levelapp.annotation.Chopp;
 import com.levelapp.annotation.Chopper;
 import com.levelapp.butterknifechopper.ButterKnifeChopperable;
-import io.reactivex.internal.subscriptions.BooleanSubscription;
+import io.realm.Realm;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.subscriptions.CompositeSubscription;
+import rx.subscriptions.Subscriptions;
 
 public class MainActivity extends AppCompatActivity {
 
-  @BindView(R.id.toolbar)
-  Toolbar toolbar;
+  @BindView(R.id.Counter_Text)
+  TextView counter;
+
+  @BindView(R.id.Random_Text)
+  TextView random;
+
+  @Chopp(chopper = {SubscriptionChopperable.class})
+  Subscription subscription = Subscriptions.empty();
+
+  @Chopp(chopper = {SubscriptionChopperable.class})
+  CompositeSubscription compositeSubscription = new CompositeSubscription();
 
   @Chopp(chopper = {ButterKnifeChopperable.class})
   Unbinder unbinder;
 
-//  @Chopp(chopper = {SubscriptionChopperable.class})
-//  Subscription subscription = Subscriptions.empty();
-//
-//  @Chopp(chopper = {SubscriptionChopperable.class})
-//  Subscription compositeSubscription = new CompositeSubscription(Subscriptions.empty());
-
-  @Chopp(chopper = {Subscription2Chopperable.class})
-  org.reactivestreams.Subscription subscription2 = new BooleanSubscription();
+  @Chopp
+  Realm realm;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Realm.init(this);
+    realm = Realm.getDefaultInstance();
     setContentView(R.layout.activity_main);
     unbinder = ButterKnife.bind(this);
-    setSupportActionBar(toolbar);
+    startCounter();
+    startRandom();
+  }
+
+  private void startRandom() {
+    final Random randomizer = new Random();
+    Subscription randomSubscription = Observable.interval(1, TimeUnit.SECONDS)
+        .map(new Func1<Long, Long>() {
+          @Override
+          public Long call(Long aLong) {
+            return aLong * randomizer.nextInt();
+          }
+        })
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Action1<Long>() {
+          @Override
+          public void call(Long rand) {
+            random.setText("Random: " + rand);
+          }
+        });
+    compositeSubscription.add(randomSubscription);
+  }
+
+  private void startCounter() {
+    subscription = Observable.interval(1, TimeUnit.SECONDS)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Action1<Long>() {
+      @Override
+      public void call(Long count) {
+        counter.setText("Count: " + count);
+      }
+    });
   }
 
   @Override
